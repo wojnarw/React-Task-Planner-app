ea8bd412519ae69c45d5191e3e19aa2d4b3d67f6
Wojciech <wojtek858@gmail.com>
2021-04-07 00:58:27 +0200

    added: toggle add time form

--- js/components/Operation.js
+++ js/components/Operation.js
@@ -2,8 +2,9 @@ import React, { useState } from "react";
 // import Operations from "../api/operations";
 import useInput from "../useInput";
 
-const Operation = ({passedOperation}) => {
+const Operation = ({ passedOperation }) => {
     const [operation] = useState(passedOperation);
+    const [showAddTimeForm, setShowAddTimeForm] = useState(false);
     const [time, propsTime] = useInput("0");
 
     const style = {
@@ -11,12 +12,16 @@ const Operation = ({passedOperation}) => {
     }
 
     const refactorTime = () => {
-        if(+operation.timeSpent / 60 >= 1) {
-            let hours = Math.floor(+operation.timeSpent / 60); 
+        if (+operation.timeSpent / 60 >= 1) {
+            let hours = Math.floor(+operation.timeSpent / 60);
             let minutes = +operation.timeSpent % 60;
             return `${hours}h ${minutes}m`;
         }
-        else return operation.timeSpent+ "m";
+        else return operation.timeSpent + "m";
+    }
+
+    const toggleAddTimeForm = () => {
+        setShowAddTimeForm(prevState => !prevState);
     }
 
     return (
@@ -26,14 +31,15 @@ const Operation = ({passedOperation}) => {
                     {operation.description}
 
                     {+operation.timeSpent > 0 && (
-                    <span className="badge badge-success badge-pill ml-2">
-                        { refactorTime() }
-                    </span>
+                        <span className="badge badge-success badge-pill ml-2">
+                            { refactorTime()}
+                        </span>
                     )}
                 </div>
 
 
-                    {/* <!-- Formularz wyświetlany po naciśnięciu "Add time", po zapisie czasu znika --> */}
+                {/* <!-- Formularz wyświetlany po naciśnięciu "Add time", po zapisie czasu znika --> */}
+                {showAddTimeForm &&
                     <form>
                         <div className="input-group input-group-sm">
                             <input type="number"
@@ -41,23 +47,25 @@ const Operation = ({passedOperation}) => {
                                 placeholder="Spent time in minutes"
                                 style={style} {...propsTime} />
                             <div className="input-group-append">
-                            <button className="btn btn-outline-success"><i className="fas fa-save"></i></button>
-                            <button className="btn btn-outline-dark"><i className="fas fa-times false"></i></button>
+                                <button className="btn btn-outline-success"><i className="fas fa-save"></i></button>
+                                <button className="btn btn-outline-dark"><i className="fas fa-times false"></i></button>
                             </div>
                         </div>
                     </form>
-
+                }
 
                 {/* <!-- div wyświetlany domyślnie, znika po wciśnięciu "Add time" --> */}
-                <div>
-                    {/* <!-- Przycisk widoczny tylko jeżeli status zadania jest "open" --> */}
-                    <button className="btn btn-outline-success btn-sm mr-2">
-                    Add time
+                {!showAddTimeForm &&
+                    <div>
+                        {/* <!-- Przycisk widoczny tylko jeżeli status zadania jest "open" --> */}
+                        <button onClick={toggleAddTimeForm} className="btn btn-outline-success btn-sm mr-2">
+                            Add time
                     <i className="fas fa-clock ml-1"></i>
-                    </button>
+                        </button>
 
-                    <button className="btn btn-outline-danger btn-sm"><i className="fas fa-trash"></i></button>
-                </div>
+                        <button className="btn btn-outline-danger btn-sm"><i className="fas fa-trash"></i></button>
+                    </div>
+                }
             </li>
         </>
     )
--- js/components/Operations.js
+++ js/components/Operations.js
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from "react";
-import {getOperationsFromDB} from "../api/operations";
+import { getOperationsFromDB } from "../api/operations";
 import Operation from "./Operation";
 
-const Operations = ({task, hasOperations, showForm}) => {
+const Operations = ({ task, hasOperations, showForm }) => {
     const [operations, setOperations] = useState([]);
 
-    const prepareOperations = async() => {
+    const prepareOperations = async () => {
         const response = await getOperationsFromDB(task.id);
-        if(response.error) console.warn("Error getting operations from database!");
+        if (response.error) console.warn("Error getting operations from database!");
         else {
             setOperations(response.data);
             hasOperations(response.data.length > 0);
@@ -16,33 +16,33 @@ const Operations = ({task, hasOperations, showForm}) => {
 
     useEffect(() => {
         prepareOperations();
-    },[]);
+    }, []);
 
     return (
         <>
-        { showForm &&
-            <div className="card-body">
-                
+            { showForm &&
+                <div className="card-body">
+
                     <form>
                         <div className="input-group">
                             <input type="text"
-                                    className="form-control"
-                                    placeholder="Operation description"/>
+                                className="form-control"
+                                placeholder="Operation description" />
 
-                                <div className="input-group-append">
-                                    <button className="btn btn-info">
+                            <div className="input-group-append">
+                                <button className="btn btn-info">
                                     Add
                                     <i className="fas fa-plus-circle ml-1"></i>
-                                    </button>
-                                </div>
+                                </button>
+                            </div>
                         </div>
                     </form>
-            </div>
-        }
+                </div>
+            }
 
             <ul className="list-group list-group-flush">
-                { operations.map((el, index) => <Operation key={index} passedOperation={el}  />) }
-                    {/* onRemove={handleRemove} onStatusChange={toggleStatus} />) } */}
+                { operations.map((el, index) => <Operation key={index} passedOperation={el} />) }
+                {/* onRemove={handleRemove} onStatusChange={toggleStatus} />) } */}
             </ul>
         </>
     )

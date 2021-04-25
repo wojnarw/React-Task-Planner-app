2dd36b2f1e1eb60e2ceae68aaa4625749cb79fc9
Wojciech <wojtek858@gmail.com>
2021-04-25 23:03:46 +0200

    prepare for build

--- js/api/constants.js
+++ js/api/constants.js
@@ -1,4 +1,4 @@
-const API_KEY = "420da211-fa14-41e2-b263-09c8e20690bf";
+const API_KEY = "355b8034-7658-4fc1-8720-14eecf6f1c09";
 const API_URL = "https://todo-api.coderslab.pl/api";
 
 export {API_KEY, API_URL};
